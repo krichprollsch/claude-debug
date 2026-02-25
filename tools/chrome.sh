847267a9a877f0e45af9a77e7a3337aadb2e1e64
Pierre Tachoire <pierre@lightpanda.io>
2026-02-25 08:30:46 +0100

    start chrome w/o sandbox for docker

--- tools/chrome.sh
+++ tools/chrome.sh
@@ -6,4 +6,5 @@ google-chrome --remote-debugging-address=0.0.0.0 \
     --remote-debugging-port=9223 --window-size=1200,800 --no-first-run \
     --user-data-dir=/tmp/chrome-tmp \
     --headless=new \
+    --no-sandbox \
     --proxy-server=http://127.0.0.1:3000 --ignore-certificate-errors
